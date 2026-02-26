e41b2a051687a6afde1114615d79d4d376f31387
AdityaSinha2911 <adityahjp11111@gmail.com>
2026-02-26 15:49:10 +0530

    File and Folder Creation Added

--- agent.py
+++ agent.py
@@ -1,4 +1,15 @@
 import requests
+import os
+
+def create_file(filename, content):
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(content)
+    return f"File {filename} created successfully."
+
+def create_folder(foldername):
+    os.makedirs(foldername, exist_ok=True)
+    return f"Folder {foldername} created."
+
 
 
 # I used ollama local LLM
@@ -22,6 +33,15 @@ def agent(user_input):
 
     decision_prompt = f"""
     You are an AI agent.
+
+    Try to answer in less words, don't explain too much, until asked.
+
+    If the user wants to create a file, respond like:
+    CREATE_FILE: filename.py
+
+    If the user wants to create a folder, respond like:
+    CREATE_FOLDER: foldername
+
     If the user is asking for current time, respond with: USE_TIME_TOOL
     Otherwise respond with: USE_LLM
 
@@ -31,7 +51,20 @@ def agent(user_input):
     decision = ask_llm(decision_prompt)
 
     if "USE_TIME_TOOL" in decision:
+
         return get_time()
+    
+    elif "CREATE_FILE:" in decision:
+
+        filename = decision.split("CREATE_FILE:")[1].strip()
+        code = ask_llm(f"Generate full Python code for {filename}")
+        return create_file(filename, code)
+    
+    elif "CREATE_FOLDER:" in decision:
+
+        foldername = decision.split("CREATE_FOLDER:")[1].strip()
+        return create_folder(foldername)
+    
     else:
         return ask_llm(user_input)
 
